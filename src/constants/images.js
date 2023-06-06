433377e29ee1789bfc7f40835aeed16f6860ebb3
Sonu <113425856+lenientsonu@users.noreply.github.com>
2023-06-06 14:32:15 +0530

    App Update
    small fixes for images and social media links etc.

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,7 +3,6 @@ import mobile from "../assets/mobile.png";
 import api from "../assets/api.png";
 import css from "../assets/css.png";
 import figma from "../assets/figma.png";
-import flutter from "../assets/flutter.png";
 import git from "../assets/git.png";
 import graphql from "../assets/graphql.png";
 import html from "../assets/html.png";
@@ -25,14 +24,6 @@ import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 
-import adidas from "../assets/adidas.png";
-import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
-import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
-import skype from "../assets/skype.png";
-import spotify from "../assets/spotify.png";
-
 import portfolio from "../assets/portfolio.jpg";
 import expenseTracker from "../assets/expense-tracker.jpg";
 import mailbox from "../assets/mailbox.jpg";
@@ -43,7 +34,6 @@ export default {
     api,
     css,
     figma,
-    flutter,
     git,
     graphql,
     html,
@@ -62,13 +52,6 @@ export default {
     profile,
     circle,
     logo,
-    adidas,
-    amazon,
-    asus,
-    bolt,
-    nb,
-    skype,
-    spotify,
     portfolio,
     expenseTracker,
     mailbox,
